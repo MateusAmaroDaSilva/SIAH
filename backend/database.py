7211fb6fea223981c52ce996c29ed7cb03b8c400
MateusAmaroDaSilva <ericaeandre44@gmaill.com>
2025-08-24 20:36:40 -0300

    Sistema Integrado de Acesso á Saúde

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 
 # Configurações do Supabase
 SUPABASE_URL = "https://bngwnknyxmhkeesoeizb.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuZ3dua255eG1oa2Vlc29laXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MzUwMDcsImV4cCI6MjA3MTExMTAwN30.MVVHAuicG_pkv0OR1h3HEwI-gx7d5hYoqX-xrK17B_U"
+SUPABASE_KEY = "SEU_SUPABASE_KEY_AQUI"
 
 # Cliente Supabase
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
