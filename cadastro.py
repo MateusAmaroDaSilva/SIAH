fd1cc21f1b30fd95c14ba6286250b130eeae9b10
MateusAmaroDaSilva <ericaeandre44@gmaill.com>
2025-09-27 18:40:44 -0300

    Sistema de Reconhecimento Facial

--- cadastro.py
+++ cadastro.py
@@ -6,7 +6,7 @@ import base64
 import numpy as np
 
 SUPABASE_URL = "https://bngwnknyxmhkeesoeizb.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuZ3dua255eG1oa2Vlc29laXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MzUwMDcsImV4cCI6MjA3MTExMTAwN30.MVVHAuicG_pkv0OR1h3HEwI-gx7d5hYoqX-xrK17B_U"
+SUPABASE_KEY = "SEU_SUPABASE_KEY_AQUI"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 SIMILARITY_THRESHOLD = 0.85
